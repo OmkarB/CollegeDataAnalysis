dbe63db31b64fa8fc4f69d3c53bc02de657c25da
Omkar Bhat <omkar1496@gmail.com>
2015-12-29 23:59:54 -0500

    Delete get_colors.R

--- get_colors.R
+++
@@ -1,5 +0,0 @@
-get_colors = function(data, color_col){
-  unique_color_values = unique(data[[color_col]])
-  unique_colors = rainbow(length(unique_color_values))
-  return(list(unique_color_values, unique_colors))
-}
